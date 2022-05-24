778e62e415e692e8b8f32de7e0a468eed6bfe9da
LilouCode <lisa.music@outlook.com>
2022-05-24 09:15:21 +0200

    feat: add navigations bar to the app

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom"
-// import Footer from "../components/Footer";
-// import Header from '../components/Header';
-import '../styles/main.scss';
+import { Outlet } from "react-router-dom";
+import "../style/main.scss";
+import NavVertical from "../components/NavVertical";
+import NavHorizontal from "../components/NavHorizontal";
 function App() {
-    return (
-      <div>
-      {/* <Header/> */}
-      <Outlet />
-      {/* <Footer/> */}
+  return (
+    <div>
+      <NavHorizontal />
+      <div className="app">
+        <NavVertical />
+        <Outlet />
+      </div>
     </div>
-    )
-  }
-  export default App
+  );
+}
+export default App;

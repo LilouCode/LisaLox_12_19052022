1e2da8826f829873f985aeed5fbc4a3259439133
LilouCode <lisa.music@outlook.com>
2022-05-28 10:39:09 +0200

    feat (radar): translate name and change axis

--- src/components/RadarPerformanceChart.jsx
+++ src/components/RadarPerformanceChart.jsx
@@ -8,28 +8,57 @@ import {
 } from "recharts";
 import { USER_PERFORMANCE } from "../__mocks__/mock";
 
+const data = USER_PERFORMANCE[1].data;
+// const kind = USER_PERFORMANCE[1].kind;
+console.log("User performances:", data);
+
+//traduction
+const frenchTranslator = {
+  1: "Cardio",
+  2: "Energie",
+  3: "Endurance",
+  4: "Force",
+  5: "Vitesse",
+  6: "Intensité",
+};
+const getTittle = (props) => {
+  const { payload, x, y } = props;
+  const value = payload.value;
+  const side = payload.coordinate;
+
+  return (
+    <text
+      x={x}
+      y={payload.index === 3 ? y + 10 : y}
+      textAnchor= {
+        side >= -30 && Math.abs(side) !== 90
+          ? "start"
+          : Math.abs(side) === 90
+          ? "middle"
+          : "end"
+      }
+      margin={5}
+      fill="white"
+      fontSize="12"
+    >
+      {frenchTranslator[value]}
+    </text>
+  );
+  // return frenchTranslator[num];
+};
+
 function RadarPerformanceChart() {
-  const data = USER_PERFORMANCE[1].data;
-  const kind = USER_PERFORMANCE[1].kind;
-  console.log("User performances:", data);
-  const getTittle = (num) => {
-    return kind[num];
-  };
   return (
     <div className="chart__square radar">
       <ResponsiveContainer height="100%" width="100%">
         <RadarChart
           data={data}
-          margin={{ top: 50, bottom: 50, left: 50, right: 50 }}
+          margin={{ top: 30, bottom: 30, left: 30, right: 30 }}
+          startAngle={30}
+          endAngle={-330}
         >
-          <PolarGrid fill="white" />
-          <PolarAngleAxis
-            dataKey="kind"
-            tickFormatter={getTittle}
-            fontSize="12"
-            color="white"
-            tickLine={false}
-          />
+          <PolarGrid radialLines={false} />
+          <PolarAngleAxis dataKey="kind" tick={getTittle} tickLine={false} />
           <PolarRadiusAxis
             angle={30}
             type="number"
@@ -41,7 +70,7 @@ function RadarPerformanceChart() {
           <Radar
             dataKey="value"
             fill="red"
-            fillOpacity={0.8}
+            fillOpacity={0.85}
             animationEasing={"ease-out"}
             stroke="none"
           />

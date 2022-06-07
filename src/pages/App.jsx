4c6466b3c321da422219a80ff2d2f67a48ab4e5c
LilouCode <lisa.music@outlook.com>
2022-06-07 13:48:51 +0200

    feat: add providers

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,15 +2,24 @@ import { Outlet } from "react-router-dom";
 import "../style/main.scss";
 import NavVertical from "../components/NavVertical";
 import NavHorizontal from "../components/NavHorizontal";
+import DevSection from "../components/DevSection";
+import { SourceProvider, DataProvider, IdProvider } from "../utils/context";
 function App() {
   return (
-    <div>
-      <NavHorizontal />
-      <div className="app">
-        <NavVertical />
-        <Outlet />
+    <SourceProvider>
+      <IdProvider>
+      <DataProvider>
+      <div>
+        <DevSection />
+        <NavHorizontal />
+        <div className="app">
+          <NavVertical />
+          <Outlet />
+        </div>
       </div>
-    </div>
+      </DataProvider>
+      </IdProvider>
+    </SourceProvider>
   );
 }
 export default App;
